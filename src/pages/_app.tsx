8cf318ae17490ea43827947b342dfcc7d199cda3
maxkrieg <maxwell.krieg@gmail.com>
2021-12-21 13:20:20 -0700

    add insane query updaters to urql client

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,66 @@
 import { ChakraProvider } from '@chakra-ui/react'
-import { Provider, createClient } from 'urql'
+import { createClient, dedupExchange, fetchExchange, Provider } from 'urql'
+import { cacheExchange, Cache, QueryInput } from '@urql/exchange-graphcache'
+import {
+  CurrentUserDocument,
+  CurrentUserQuery,
+  LoginMutation,
+  RegisterMutation
+} from '../generated/graphql'
+
+function betterUpdateQuery<Result, Query>(
+  cache: Cache,
+  queryInput: QueryInput,
+  result: any,
+  updateFunc: (r: Result, q: Query) => Query
+) {
+  return cache.updateQuery(queryInput, data => updateFunc(result, data as any) as any)
+}
 
 const client = createClient({
   url: 'http://localhost:4000/graphql',
+  exchanges: [
+    dedupExchange,
+    cacheExchange({
+      updates: {
+        Mutation: {
+          login: (queryResult, _args, cache, _info) => {
+            betterUpdateQuery<LoginMutation, CurrentUserQuery>(
+              cache,
+              { query: CurrentUserDocument },
+              queryResult,
+              (result, query) => {
+                if (result.login.errors) {
+                  return query
+                } else {
+                  return {
+                    currentUser: result.login.user
+                  }
+                }
+              }
+            )
+          },
+          register: (queryResult, _args, cache, _info) => {
+            betterUpdateQuery<RegisterMutation, CurrentUserQuery>(
+              cache,
+              { query: CurrentUserDocument },
+              queryResult,
+              (result, query) => {
+                if (result.register.errors) {
+                  return query
+                } else {
+                  return {
+                    currentUser: result.register.user
+                  }
+                }
+              }
+            )
+          }
+        }
+      }
+    }),
+    fetchExchange
+  ],
   fetchOptions: {
     credentials: 'include'
   }
