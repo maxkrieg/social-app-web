54312b68934c84fd04124acb70112206877a244f
maxkrieg <maxwell.krieg@gmail.com>
2022-12-04 10:29:42 -0500

    refactor create event, add datepicker

--- src/pages/create-event.tsx
+++ src/pages/create-event.tsx
@@ -4,10 +4,11 @@ import { Form, Formik } from 'formik'
 import { NextPage } from 'next'
 import { withUrqlClient } from 'next-urql'
 import { useRouter } from 'next/router'
+import { CombinedError } from 'urql'
 
 import { InputField } from '../components/InputField'
 import Layout from '../components/Layout'
-import { useCreateEventMutation } from '../generated/graphql'
+import { CreateEventMutation, useCreateEventMutation } from '../generated/graphql'
 import { createUrqlClient } from '../utils/createUrqlClient'
 import { useIsAuth } from '../utils/useIsAuth'
 
@@ -19,53 +20,77 @@ const CreateEvent: NextPage<Props> = () => {
   useIsAuth()
   const [_, createEvent] = useCreateEventMutation()
 
+  const handleCreateEventSuccess = (data: CreateEventMutation | undefined) => {
+    toast({
+      title: 'Event created',
+      status: 'success',
+      duration: 3000,
+      isClosable: true
+    })
+    const { id: eventId } = data?.createEvent || {}
+    if (eventId) {
+      router.push(`/event/${eventId}`)
+    } else {
+      router.push('/')
+    }
+  }
+
+  const handleCreateEventError = (error: CombinedError | undefined) => {
+    console.error(error)
+    toast({
+      title: 'Error creating event',
+      status: 'error',
+      duration: 3000,
+      isClosable: true
+    })
+  }
+
   return (
     <Layout variant='small'>
       <Heading size='lg' mb={4}>
         Create Event
       </Heading>
       <Formik
-        initialValues={{ title: '', location: '', description: '' }}
+        initialValues={{ title: '', location: '', description: '', dateTime: '' }}
         onSubmit={async values => {
-          const { error } = await createEvent({ input: values })
+          const { error, data } = await createEvent({ input: values })
           if (!error) {
-            toast({
-              title: 'Event created',
-              status: 'success',
-              duration: 3000,
-              isClosable: true
-            })
-            router.push('/')
+            handleCreateEventSuccess(data)
           } else {
-            console.error(error)
-            toast({
-              title: 'Error creating event',
-              status: 'error',
-              duration: 3000,
-              isClosable: true
-            })
+            handleCreateEventError(error)
           }
         }}
       >
-        {({ isSubmitting }) => (
-          <Form>
-            <InputField name='title' placeholder='Event title' label='Title' />
-            <Box mt={4}>
-              <InputField name='location' placeholder='Event location' label='Location' />
-            </Box>
-            <Box mt={4}>
-              <InputField
-                name='description'
-                label='Description'
-                placeholder='Description...'
-                textarea
-              />
-            </Box>
-            <Button mt={4} type='submit' isLoading={isSubmitting} colorScheme='teal'>
-              Create
-            </Button>
-          </Form>
-        )}
+        {({ isSubmitting, values }) => {
+          console.log('values', values)
+          return (
+            <Form>
+              <InputField name='title' placeholder='Event title' label='Title' />
+              <Box mt={4}>
+                <InputField name='location' placeholder='Event location' label='Location' />
+              </Box>
+              <Box mt={4}>
+                <InputField
+                  name='dateTime'
+                  placeholder='Select Date and Time'
+                  label='Date and Time'
+                  type='datetime-local'
+                />
+              </Box>
+              <Box mt={4}>
+                <InputField
+                  name='description'
+                  label='Description'
+                  placeholder='Description...'
+                  textarea
+                />
+              </Box>
+              <Button mt={4} type='submit' isLoading={isSubmitting} colorScheme='teal'>
+                Create
+              </Button>
+            </Form>
+          )
+        }}
       </Formik>
     </Layout>
   )
