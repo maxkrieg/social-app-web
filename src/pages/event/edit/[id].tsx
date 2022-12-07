2e487c2b8e7bfac3d1e157aa1978d391b946ac19
maxkrieg <maxwell.krieg@gmail.com>
2022-12-07 16:01:36 -0500

    wip getting event date time to show properly in form

--- src/pages/event/edit/[id].tsx
+++ src/pages/event/edit/[id].tsx
@@ -3,6 +3,7 @@ import { Formik, Form } from 'formik'
 import { withUrqlClient } from 'next-urql'
 import { useRouter } from 'next/router'
 import React from 'react'
+import { format } from 'date-fns'
 import { InputField } from '../../../components/InputField'
 import Layout from '../../../components/Layout'
 import {
@@ -20,9 +21,11 @@ interface EditEventValues {
   title: string
   location: string
   description: string
+  dateTime: string
 }
 
 const EditEvent: React.FC<Props> = () => {
+  console.log('---------------------------------------')
   const router = useRouter()
   const toast = useToast()
   const [{ data, fetching }] = useGetEvent()
@@ -32,6 +35,8 @@ const EditEvent: React.FC<Props> = () => {
   const [, updateEvent] = useUpdateEventMutation()
   const [, deleteEvent] = useDeleteEventMutation()
   const { event } = data || {}
+
+  console.log('event data', event)
 
   const handleUpdateEvent = async (values: EditEventValues) => {
     if (!event) return
@@ -83,10 +88,15 @@ const EditEvent: React.FC<Props> = () => {
     return <Layout>Could not find event</Layout>
   }
 
-  if (event.user.id !== userData?.currentUser?.id) {
+  const eventHost = event?.eventUsers.find(
+    eventUser => eventUser.user.id === userData?.currentUser?.id
+  )
+  if (eventHost?.user.id !== userData?.currentUser?.id) {
     return <Layout>Not authorized to edit this event</Layout>
   }
-
+  console.log('event dateTime', event.dateTime)
+  const formattedDatetime = format(new Date(event.dateTime), "yyyy-MM-dd'T'HH:mm:ss")
+  console.log({ formattedDatetime })
   return (
     <Layout variant='small'>
       <Heading size='lg' mb={4}>
@@ -97,35 +107,47 @@ const EditEvent: React.FC<Props> = () => {
           {
             title: event.title,
             location: event.location,
-            description: event.description
+            description: event.description,
+            dateTime: formattedDatetime
           } as EditEventValues
         }
         onSubmit={handleUpdateEvent}
       >
-        {({ isSubmitting }) => (
-          <Form>
-            <InputField name='title' placeholder='Event title' label='Title' />
-            <Box mt={4}>
-              <InputField name='location' placeholder='Event location' label='Location' />
-            </Box>
-            <Box mt={4}>
-              <InputField
-                name='description'
-                label='Description'
-                placeholder='description...'
-                textarea
-              />
-            </Box>
-            <Flex flex={1}>
-              <Button mt={4} type='submit' isLoading={isSubmitting} colorScheme='teal'>
-                Update
-              </Button>
-              <Button mt={4} ml='auto' onClick={handleDeleteEvent}>
-                Delete
-              </Button>
-            </Flex>
-          </Form>
-        )}
+        {({ isSubmitting, values }) => {
+          console.log('form values', values)
+          return (
+            <Form>
+              <InputField name='title' placeholder='Event title' label='Title' />
+              <Box mt={4}>
+                <InputField name='location' placeholder='Event location' label='Location' />
+              </Box>
+              <Box mt={4}>
+                <InputField
+                  name='dateTime'
+                  placeholder='Select Date and Time'
+                  label='Date and Time'
+                  type='datetime-local'
+                />
+              </Box>
+              <Box mt={4}>
+                <InputField
+                  name='description'
+                  label='Description'
+                  placeholder='description...'
+                  textarea
+                />
+              </Box>
+              <Flex flex={1}>
+                <Button mt={4} type='submit' isLoading={isSubmitting} colorScheme='teal'>
+                  Update
+                </Button>
+                <Button mt={4} ml='auto' onClick={handleDeleteEvent}>
+                  Delete
+                </Button>
+              </Flex>
+            </Form>
+          )
+        }}
       </Formik>
     </Layout>
   )
